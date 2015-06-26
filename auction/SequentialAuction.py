6ecf4af2d7f6fbf0154475697815ceb9d265660c
csol3108 <takehiro.oyakawa@gmail.com>
2015-06-26 17:39:16 -0400

    Created a SequentialAuction class.  This class takes a list of bidders and the number of rounds that will occur in the auction.  In each round, bidders place their bids and the bidder with the highest bid will pay the second highest price.  The testing functions now rely on this class.

---
+++ auction/SequentialAuction.py
@@ -0,0 +1,76 @@
+"""
+Implements a class that runs a sequential auction of homogeneous goods, where one good is auctioned off in each
+round.  Bidders pay the second highest price in each round.
+"""
+from heapq import nlargest
+
+
+class SequentialAuction:
+    """Sequential auction where the bidder with the highest bid in each round pays the second highest price.
+    """
+    def __init__(self, bidders, num_rounds):
+        """
+        :param bidders: List.  A list of bidders participating in the auction.
+        :param num_rounds: Integer.  The number of rounds the auction will run for.
+        """
+        self.bidders = bidders
+        self.num_bidders = len(bidders)
+        self.num_rounds = num_rounds
+        self.bids = [[0] * self.num_bidders for r in range(num_rounds)]
+        self.winners = [0] * num_rounds
+        self.payments = [0] * num_rounds
+        self.total_revenue = 0.0
+
+    def run(self):
+        """
+        Runs the auction to completion.
+        """
+        for r in range(self.num_rounds):
+            round_number = r + 1
+            # Place bids
+            for i in range(self.num_bidders):
+                self.bids[r][i] = self.bidders[i].place_bid(round_number)
+            # Winner determination
+            self.winners[r] = self.bids[r].index(max(self.bids[r]))
+            # Determine payment
+            self.payments[r] = float(nlargest(2, self.bids[r])[1])
+            self.total_revenue += self.payments[r]
+            # Notify winning bidder
+            for i, b in enumerate(self.bidders):
+                if i == self.winners[r]:
+                    self.bidders[i].set_round_result(round_number, True, self.payments[r])
+                else:
+                    self.bidders[i].set_round_result(round_number, False, 0.0)
+
+    def print_bidder_results(self):
+        """
+        Prints out the informatin of each bidder for each round.
+        """
+        print('Bidder Information')
+        for i, bidder in enumerate(self.bidders):
+            print('Bidder', i, ' v =', bidder.valuations)
+            print('Win Result:', bidder.win)
+            print('Valuation:', bidder.valuations)
+            print('Payment:', bidder.payment)
+            print('Utility:', bidder.utility)
+            print('Number of goods procured:', bidder.num_goods_won)
+
+    def print_round_overview(self):
+        """
+        Prints out a high-level overview of the results of each round.
+        """
+        print('Round overview')
+        for r in range(self.num_rounds):
+            round_number = r + 1
+            print('Auction round', round_number)
+            print('Winner: Bidder', self.winners[r])
+            print('Payment:', self.payments[r])
+
+    def print_summary(self):
+        """
+        Prints out a summary of the result of the auction.
+        """
+        print('Summary')
+        print('Winners:', self.winners)
+        print('Payments:', self.payments)
+        print('Total Revenue:', self.total_revenue)
--- auction/test_simple_bidder.py
+++ auction/test_simple_bidder.py
@@ -1,8 +1,8 @@
 """
 Test the SimpleBidder implementation
 """
+from auction.SequentialAuction import SequentialAuction
 from bidder.simple import SimpleBidder
-from heapq import nlargest
 import random
 import numpy
 
@@ -17,56 +17,13 @@ num_bidders = 5
 possible_types = [i / 100.0 for i in range(101)]
 type_dist = [1 / 101.0 for i in range(101)]
 
-# Initialize list to store auction outcome
-bids = [[0] * num_bidders for r in range(num_rounds)]
-winners = [0] * num_rounds
-payments = [0] * num_rounds
-total_revenue = 0.0
-
 # Generate bidders
 bidders = [SimpleBidder(i, num_rounds, num_bidders, possible_types, type_dist)
            for i in range(num_bidders)]
 
 # Run auction
-for r in range(num_rounds):
-    round_number = r + 1
-    # Place bids
-    bids[r] = [bidders[i].place_bid(round_number) for i in range(num_bidders)]
-    # Determine winner
-    winners[r] = bids[r].index(max(bids[r]))
-    # Determine payment
-    payments[r] = float(nlargest(2, bids[r])[1])
-    total_revenue += payments[r]
-    # Notify winning bidder
-    for i, b in enumerate(bidders):
-        if i == winners[r]:
-            bidders[i].set_round_result(round_number, True, payments[r])
-        else:
-            bidders[i].set_round_result(round_number, False, 0.0)
-
-# Display outcome to terminal
-print('Bidder information')
-for i in range(num_bidders):
-    print('Bidder', i, ' v =', bidders[i].valuations)
-for r in range(num_rounds):
-    round_number = r + 1
-    print('Auction round', round_number)
-    print('Bids placed')
-    for i in range(num_bidders):
-        print('Bidder', i, 'b =', bids[r][i])
-    print('Winner: Bidder', winners[r])
-    print('Payment:', payments[r])
-
-print('Summary')
-print('Winners:', winners)
-print('Payments:', payments)
-print('Total Revenue:', total_revenue)
-
-print('Bidder Data')
-for i, b in enumerate(bidders):
-    print('Bidder', i)
-    print('Win Result:', b.win)
-    print('Valuation:', b.valuations)
-    print('Payment:', b.payment)
-    print('Utility:', b.utility)
-    print('Number of goods procured:', b.num_goods_won)
+auction = SequentialAuction(bidders, num_rounds)
+auction.run()
+auction.print_summary()
+auction.print_round_overview()
+auction.print_bidder_results()
